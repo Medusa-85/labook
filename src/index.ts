ef591d3577e55f0ad58838fdf8a465345ed89eca
Medusa-85 <lima.mariana012@gmail.com>
2023-02-13 12:39:47 -0300

    router e controller de 'users'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import Knex from "knex";
+import { userRouter } from "./routers/userRouter";
+import { postRouter } from "./routers/postRouter";
 
 const app = express()
 
@@ -8,34 +9,5 @@ app.use(cors())
 app.use(express.json())
 app.listen(3003, ()=>{console.log(`Servidor rodando na porta ${3003}`)})
 
-app.get("/users", async (req: Request, res: Response)=>{
-    try{
-        const db = Knex({
-            client: "sqlite3",
-            connection: {
-                filename: "./src/database/labook.db",
-            },
-            useNullAsDefault: true,
-            pool: { 
-                min: 0,
-                max: 1,
-                afterCreate: (conn: any, cb: any) => {
-                    conn.run("PRAGMA foreign_keys = ON", cb)
-                }
-            }
-        })
-
-        const output = await db("users").select()
-
-        res.status(200).send(output)
-        
-    } catch (error) {
-        console.log(error)
-
-        if(error instanceof Error) {
-            res.status(500).send(error.message)
-        } else {
-            res.status(500).send("Erro inesperado")
-        }
-    }
-})
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
